5ddd33e53dc03ad4d1ad55eb8bab964600dd9eff
lrea17 <71747396+lrea17@users.noreply.github.com>
2021-03-05 13:34:22 -0800

    JsonReader tests added

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -1,9 +1,13 @@
 package persistence;
 
 import org.junit.jupiter.api.Test;
+import ski.model.Accounts;
+import ski.model.Guest;
 
 import java.io.IOException;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class JsonReaderTest extends JsonTest {
@@ -12,10 +16,38 @@ public class JsonReaderTest extends JsonTest {
     void testReaderNonExistentFile() {
         JsonReader reader = new JsonReader("./data/noSuchFile.json");
         try {
-            WorkRoom wr = reader.read();
+            Accounts acc = reader.read();
             fail("IOException expected");
         } catch (IOException e) {
             // pass
         }
     }
+
+    @Test
+    void testReaderEmptyAccounts() {
+        JsonReader reader = new JsonReader("./data/testReaderEmptyWorkRoom.json");
+        try {
+            Accounts acc = reader.read();
+            assertEquals("Snowy Mountain", acc.getName());
+            assertEquals(0, acc.numGuests());
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+    @Test
+    void testReaderGeneralAccounts() {
+        JsonReader reader = new JsonReader("./data/testReaderGeneralWorkRoom.json");
+        try {
+            Accounts acc = reader.read();
+            assertEquals("My work room", acc.getName());
+            List<Guest> listOfGuests = acc.getListOfGuests();
+            assertEquals(2, listOfGuests.size());
+            checkGuest("A", 28, listOfGuests.get(0));
+            checkGuest("B", 65, listOfGuests.get(1));
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
 }

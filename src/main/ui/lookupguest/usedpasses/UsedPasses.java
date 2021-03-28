e7702c7ee742a1cd3734d36440be0d67ab7a5eff
lrea17 <71747396+lrea17@users.noreply.github.com>
2021-03-28 13:59:10 -0700

    got scroll list to work with numbers

--- src/main/ui/lookupguest/usedpasses/UsedPasses.java
+++ src/main/ui/lookupguest/usedpasses/UsedPasses.java
@@ -19,6 +19,8 @@ public class UsedPasses extends ApplicationButtons {
     private Guest guest;
     private JPanel usedPassesPane = new JPanel(new GridLayout(0, 1));
     private JDialog usedPassesDialog = new JDialog();
+    private JScrollPane scrollPane = new JScrollPane();
+    private JList listOfExpiredPass;
 
     public UsedPasses(SkiAppGUI editor, JComponent parent) {
         super(editor, parent);
@@ -67,21 +69,46 @@ public class UsedPasses extends ApplicationButtons {
         Font font = new Font("Arial", Font.BOLD, 12);
         usedPassesTitle.setFont(font);
         usedPassesPane.add(usedPassesTitle, BorderLayout.BEFORE_FIRST_LINE);
-
         usedPassesPane.setBackground(Color.WHITE);
 
         //usedPasses.setText("Used passes: ");
-        JList listOfExpiredPass = new JList(guest.getListOfExpiredPasses().toArray());
-        usedPassesPane.add(listOfExpiredPass);
-        usedPassesPane.setVisible(true);
 
+        setScrollPaneSetUp();
+
+        //usedPassesPane.add(listOfExpiredPass);
+        usedPassesPane.add(scrollPane);
+        scrollPane.setVisible(true);
+        usedPassesPane.setVisible(true);
         usedPassesDialog.add(usedPassesPane);
         //editor.setSidePanel(usedPassesPane);
     }
 
-    public JList newLineListItems() {
-        JList listOfExpiredPass = new JList(guest.getListOfExpiredPasses().toArray());
-        return listOfExpiredPass;
+    public void newLineListItems() {
+        listOfExpiredPass = new JList(guest.getListOfExpiredPasses().toArray());
+    }
+
+
+    public void setScrollPaneSetUp() {
+        newLineListItems();
+
+
+        listOfExpiredPass.setModel(new AbstractListModel() {
+
+            private static final long serialVersionUID = 1L;
+
+            ArrayList<Pass> strings = guest.getListOfExpiredPasses();
+
+            public int getSize() {
+                return strings.size();
+            }
+
+            public Object getElementAt(int i) {
+                return (i + 1) + ": " + strings.get(i).toString();
+            }
+        });
+
+        usedPassesPane.add(listOfExpiredPass);
+        scrollPane.setViewportView(listOfExpiredPass);
     }
 
 

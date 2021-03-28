aed416a95e6e4175abd3380d431439885ecefd67
lrea17 <71747396+lrea17@users.noreply.github.com>
2021-03-28 11:42:35 -0700

    added pop up window with used passes

--- src/main/ui/lookupguest/usedpasses/UsedPasses.java
+++ src/main/ui/lookupguest/usedpasses/UsedPasses.java
@@ -7,6 +7,7 @@ import ui.lookupguest.LookUpGuestPopUp;
 import ui.lookupguest.LookupGuest;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -14,6 +15,8 @@ import java.awt.event.ActionListener;
 public class UsedPasses extends ApplicationButtons {
     private SkiAppGUI editor;
     private Guest guest;
+    private JPanel usedPassesPane = new JPanel(new GridLayout(0, 1));
+    private JLabel usedPasses = new JLabel("Used passes: ");
 
     public UsedPasses(SkiAppGUI editor, JComponent parent) {
         super(editor, parent);
@@ -36,11 +39,37 @@ public class UsedPasses extends ApplicationButtons {
         public void actionPerformed(ActionEvent e) {
             LookupGuest.playSound(LookupGuest.getClickSound());
             getUsedPasses();
+            showUsedPasses();
         }
     }
 
     public void setGuest(Guest guest) {
         this.guest = guest;
+    }
+
+    //TODO add name of guest in
+    public void showUsedPasses() {
+        JDialog usedPassesPopUp = new JDialog();
+        usedPassesPopUp.setMinimumSize(new Dimension(200,200));
+        usedPassesPopUp.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        usedPassesPopUp.setVisible(true);
+        usedPassesPopUp.setLayout(new BorderLayout());
+        usedPassesPopUp.setTitle(guest.getName() + "'s Used Passes: ");
+
+        //TODO I dont think i need this it makes double title int he box
+        //JLabel title = new JLabel(guest.getName() + "'s Used Passes: ");
+        //Font font = new Font("Arial", Font.BOLD, 12);
+        //title.setFont(font);
+        //usedPassesPane.add(title);
+
+        usedPassesPane.setBackground(Color.WHITE);
+
+        usedPassesPane.add(usedPasses);
+        usedPasses.setText("Used passes: " + guest.getListOfExpiredPasses());
+        usedPassesPane.setVisible(true);
+
+        usedPassesPopUp.add(usedPassesPane);
+        //editor.setSidePanel(usedPassesPane);
     }
 
     private void getUsedPasses() {
